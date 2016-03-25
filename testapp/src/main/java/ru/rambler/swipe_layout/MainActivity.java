3d76fd08bb4a152d324debcd6acfb363520a3282
Sergeev Eduard <e.sergeev@rambler-co.ru>
2016-03-25 17:28:38 +0300

    improve sample layouts

--- testapp/src/main/java/ru/rambler/swipe_layout/MainActivity.java
+++ testapp/src/main/java/ru/rambler/swipe_layout/MainActivity.java
@@ -3,6 +3,8 @@ package ru.rambler.swipe_layout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ru.rambler.libs.swipe_layout.SwipeLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
