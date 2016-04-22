4dac3932b1568518b53757429be72197be27d35e
Sergeev Eduard <e.sergeev@rambler-co.ru>
2016-04-22 19:40:45 +0300

    update support libs to V23.3.0

--- testapp/src/main/java/ru/rambler/swipe_layout/MainActivity.java
+++ testapp/src/main/java/ru/rambler/swipe_layout/MainActivity.java
@@ -1,15 +1,13 @@
 package ru.rambler.swipe_layout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import ru.rambler.libs.swipe_layout.SwipeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
